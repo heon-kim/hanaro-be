867ebe9a215fd826fdd4eaf9d41c0e5738485576
heon-kim <vndtka123@gmail.com>
2024-11-21 15:19:11 +0900

    모든 회원 보기 요청 처리(mySQL)

--- jsp/src/main/java/com/example/demo1/MemberController.java
+++ jsp/src/main/java/com/example/demo1/MemberController.java
@@ -1,5 +1,8 @@
 package com.example.demo1;
 
+import com.example.demo1.vo.MemberDAO;
+import com.example.demo1.vo.Members;
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -7,6 +10,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("*.action")
 public class MemberController extends HttpServlet {
@@ -25,5 +29,19 @@ public class MemberController extends HttpServlet {
             System.out.println("join");
             System.out.println("=====================================");
         }
+        else if(cmd.equals("/member/list.action")){
+            System.out.println("list");
+            System.out.println("=====================================");
+            list(request,response);
+        }
+    }
+
+    private void list(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        ServletContext application = request.getServletContext();
+        MemberDAO dao = new MemberDAO(application);
+        List<Members> mList = dao.getList();
+        dao.close();
+        request.setAttribute("mList", mList);
+        request.getRequestDispatcher("/member/memberList.jsp").forward(request,response);
     }
 }
