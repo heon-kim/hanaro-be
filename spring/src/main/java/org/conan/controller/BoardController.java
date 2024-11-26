3c7de45b5468cace8e860b7d15ea919a0bb94a63
heon-kim <vndtka123@gmail.com>
2024-11-26 15:23:45 +0900

    controller

--- spring/src/main/java/org/conan/controller/BoardController.java
+++ spring/src/main/java/org/conan/controller/BoardController.java
@@ -2,11 +2,15 @@ package org.conan.controller;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
+import org.conan.domain.Board;
 import org.conan.service.BoardService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @Log4j
@@ -18,5 +22,33 @@ public class BoardController {
     public void list(Model model){
         log.info("list");
         model.addAttribute("bList", boardService.getList());
+    }
+    @PostMapping("/write")
+    public String register(Board board, RedirectAttributes rttr){
+        log.info("write: "+board);
+        boardService.write(board);
+        rttr.addFlashAttribute("result", board.getBno());
+        return "redirect:/board/list";
+    }
+    @GetMapping("/read")
+    public void read(@RequestParam("bno") Integer bno, Model model){
+        log.info("/read");
+        model.addAttribute("board", boardService.read(bno));
+    }
+    @PostMapping("/modify")
+    public String modify(Board board, RedirectAttributes rttr){
+        log.info("modify: "+board);
+        if(boardService.modify(board)){
+            rttr.addFlashAttribute("result","success");
+        }
+        return "redirect:/board/list";
+    }
+    @PostMapping("/remove")
+    public String remove(@RequestParam("bno")Integer bno, RedirectAttributes rttr){
+        log.info("remove.........."+bno);
+        if(boardService.remove(bno)){
+            rttr.addFlashAttribute("result", "success");
+        }
+        return "redirect:/board/list";
     }
 }
--- spring/src/test/java/org/conan/time/BoardControllerTest.java
+++ spring/src/test/java/org/conan/time/BoardControllerTest.java
@@ -31,6 +31,38 @@ public class BoardControllerTest {
     }
     @Test
     public void testList() throws Exception {
-        log.info(mockMvc.perform(MockMvcRequestBuilders.get("/board/list")).andReturn().getModelAndView().getModelMap());
+        log.info(mockMvc.perform(MockMvcRequestBuilders.get("/board/list"))
+                .andReturn().getModelAndView().getModelMap());
+    }
+    @Test
+    public void testRegister() throws Exception {
+        String resultPage = mockMvc.perform(MockMvcRequestBuilders.post("/board/write")
+                .param("title","test from controller")
+                .param("content","test for controller")
+                .param("writer","user1234"))
+                .andReturn().getModelAndView().getViewName();
+        log.info(resultPage);
+    }
+    @Test
+    public void testRead() throws Exception {
+        log.info(mockMvc.perform(MockMvcRequestBuilders.get("/board/read")
+                .param("bno","16"))
+                .andReturn().getModelAndView().getModelMap());
+    }
+    @Test
+    public void testModify() throws Exception{
+        String resultPage = mockMvc.perform(MockMvcRequestBuilders.post("/board/modify")
+                .param("bno","1")
+                .param("title","일번일번")
+                .param("content", "일번 일번")
+                .param("writer","일번 유저"))
+                .andReturn().getModelAndView().getViewName();
+        log.info(resultPage);
+    }
+    @Test
+    public void testRemove() throws Exception{
+        String resultPage = mockMvc.perform(MockMvcRequestBuilders.post("/board/remove")
+                .param("bno","6")).andReturn().getModelAndView().getViewName();
+        log.info(resultPage);
     }
 }
