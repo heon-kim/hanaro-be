5a01938e81bdc562a1aad562332a53023366c2a0
heon-kim <vndtka123@gmail.com>
2024-11-27 13:21:50 +0900

    exLayout fragments

--- myBoot/src/main/java/org/conan/myboot/controller/SampleController.java
+++ myBoot/src/main/java/org/conan/myboot/controller/SampleController.java
@@ -52,5 +52,9 @@ public class SampleController {
     public void ex3(){
         log.info("ex3...............");
     }
+    @GetMapping("/exLayout1")
+    public void exLayout1(){
+        log.info("exLayout1..........");
+    }
 
 }
