0a8f494f2dd6153d8dd1ae57999e1b68c737b611
heon-kim <vndtka123@gmail.com>
2024-11-27 13:38:33 +0900

    exLayout2 fragements target

--- myBoot/src/main/java/org/conan/myboot/controller/SampleController.java
+++ myBoot/src/main/java/org/conan/myboot/controller/SampleController.java
@@ -52,7 +52,7 @@ public class SampleController {
     public void ex3(){
         log.info("ex3...............");
     }
-    @GetMapping("/exLayout1")
+    @GetMapping({"/exLayout1", "/exLayout2"})
     public void exLayout1(){
         log.info("exLayout1..........");
     }
